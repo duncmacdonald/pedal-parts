4539a317155b5d49959ead2ddd1116d4e102cca1
Duncan Macdonald <dunc.macdonald@gmail.com>
2023-10-06 21:13:00 -0700

    removing the vercel stuff

--- app/pedal/page.tsx
+++ app/pedal/page.tsx
@@ -4,7 +4,7 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-        <h1>Pedal Parts</h1>  
+      <h1>Pedal</h1>
     </main>
   )
 }
